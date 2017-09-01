95445aa4b7342e240146f772b71c3d0aace5fb17
Greg <greg@Gregs-MacBook-Pro.local>
2017-09-01 10:07:26 -0400

    moved Express URL error handling into a middleware

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
 const moment = require('moment')
 const express = require('express')
 const pug = require('pug')
+const middleware = require('./middleware')
 
 const compiledTemplate = pug.compileFile('template/template.pug')
 const app = express()
 
 // Error Handling to stop broken params, we're talking beyond just invalid
-app.use((req, res, next) => {
-  var err = null;
-  try {
-    decodeURIComponent(req.path)
-  }
-  catch (e) {
-    err = e;
-  }
-  if (err) {
-    console.log(err, req.url);
-    return res.redirect(['https://', req.get('Host'), '/404'].join(''));
-  }
-
-  //Once the error handler is done, next() will jump to the next function
-  next()
-})
+app.use(middleware.malformedUrl)
 
 app.get('/:date', (req, res) => {
   let date = req.params.date
